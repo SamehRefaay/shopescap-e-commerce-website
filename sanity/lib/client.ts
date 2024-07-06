95ec0af4b04afcf087e8153016f416e411f9e0a0
SamehRefaay <samehrefaay89@gmail.com>
2024-07-06 22:20:13 +0300

    fixing typo

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,7 +1,6 @@
 import { createClient, groq } from 'next-sanity';
 
 import { apiVersion, dataset, projectId, token, useCdn } from '../env';
-import { ProductProps } from '../../src/lib/type';
 
 export const client = createClient({
 	apiVersion,
